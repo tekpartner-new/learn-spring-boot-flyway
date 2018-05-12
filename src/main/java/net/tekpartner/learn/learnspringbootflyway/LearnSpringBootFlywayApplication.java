cc9d32ee3a600207681ec60aa2e0a66131735eea
Chandrashekar R. Gaajula <chandra.gaajula@TEKPartner.net>
2018-05-11 17:36:41 -0700

    first commit

---
+++ src/main/java/net/tekpartner/learn/learnspringbootflyway/LearnSpringBootFlywayApplication.java
@@ -0,0 +1,12 @@
+package net.tekpartner.learn.learnspringbootflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootFlywayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LearnSpringBootFlywayApplication.class, args);
+  }
+}
---
+++ src/test/java/net/tekpartner/learn/learnspringbootflyway/LearnSpringBootFlywayApplicationTests.java
@@ -0,0 +1,16 @@
+package net.tekpartner.learn.learnspringbootflyway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnSpringBootFlywayApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
